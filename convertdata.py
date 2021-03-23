361f79b78a497623f7220c017477ce84e5dae813
Erecik1 <eret112@gmail.com>
2021-03-23 23:31:12 +0100

    first commit

---
+++ convertdata.py
@@ -0,0 +1,8 @@
+class ConvertData:
+
+    def __init__(self, mouse_events_array, keyboard_events_array):
+        self.mouse_events_array = mouse_events_array
+        self.keyboard_events_array = keyboard_events_array
+
+    def convert_to_one_list(self):
+        pass
