de6306dce887a3135b560516b568c574ad9fa85c
Erecik1 <eret112@gmail.com>
2021-03-27 02:53:37 +0100

    reduce code

--- convertdata.py
+++ convertdata.py
@@ -1,8 +1,7 @@
 class ConvertData:
 
-    def __init__(self, mouse_events_array, keyboard_events_array):
-        self.mouse_events_array = mouse_events_array
-        self.keyboard_events_array = keyboard_events_array
+    def __init__(self, event_list):
+        self.event_list = event_list
 
-    def convert_to_one_list(self,key):
-        print(key)
+    def sort_data(self):
+        pass
