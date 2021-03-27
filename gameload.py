2f94b8a6a5aa7b508430b61103a6899f0a538d6f
Erecik1 <eret112@gmail.com>
2021-03-27 02:54:49 +0100

    Added informations about current stage

--- gameload.py
+++ gameload.py
@@ -19,9 +19,12 @@ class Game:
     #check is game active window if not check procces list
     def is_game_still_in_proggress(self):
         if GetWindowText(GetForegroundWindow()) == self.game_name:
+            print("Window is active")
             return True
         else:
             for process in psutil.process_iter():
                 if self.game_procces == process.name():
+                    print("Windows minimalized")
                     return False
             self.is_game_procces_active = False
+            print("Game over")
