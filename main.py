eff47dd7fcf4066bd73945ecf4b11b3702b49872
Erecik <eret112@gmail.com>
2021-04-03 17:57:00 +0200

    save method implemented

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import getdata
 import convertdata
 import gameload
+import sys
 
 #game = gameload.Game("League of Legends (TM) Client","League of Legends.exe")
 game = gameload.Game("League of Legends","LeagueClient.exe")
 data = getdata.DataCollector(game)
+output = data.save()
+convert = convertdata.ConvertData(output)
