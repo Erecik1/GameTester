5d12a4cc962d1d67df71423358e677e23273773d
Erecik <eret112@gmail.com>
2021-04-03 17:57:49 +0200

    Added gathering data from api

--- convertdata.py
+++ convertdata.py
@@ -1,7 +1,73 @@
+import requests
+import time
+import json
+
 class ConvertData:
 
     def __init__(self, event_list):
         self.event_list = event_list
+        self.champion_played = ""
+        with open('login_data.json') as f:
+            data = json.load(f)
+        self.summoner_name = data['summoner_name']
+        self.server = data['server']
+        self.api_key = data['api_key']
+        self.API_URL = f"https://{self.server}.api.riotgames.com/"
+        self.headers ={
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.83 Safari/537.1",
+        "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": self.api_key
+        }
+        self.summoner_id = self._get_summoner_id()
+        self.last_match_id = self._get_last_match_id(self.summoner_id)
+        self.match_data = self._get_match_data_by_id(self.last_match_id)
+        self.match_results = 
+    def _one_day_ago_in_epoch(self):
+        now = time.time()
+        #days before now
+        secs_in_day = 5*24*60*60
+        epoch_time = now - secs_in_day
+        #riot api needs time in milliseconds
+        epoch_time *= 1000
+        return int(epoch_time)
 
-    def sort_data(self):
-        pass
+    def _get_summoner_id(self):
+        SUMMONER_ID_URL = f"/lol/summoner/v4/summoners/by-name/{self.summoner_name}"
+        url = self.API_URL + SUMMONER_ID_URL
+        r = requests.get(url, headers=self.headers)
+        output = r.json()['accountId']
+        return output
+
+    def _get_last_match_id(self,summoner_id):
+        MATCH_URL = f"/lol/match/v4/matchlists/by-account/{summoner_id}?beginTime={self._one_day_ago_in_epoch()}"
+        url = self.API_URL + MATCH_URL
+        #waiting for riot servers to sync match_data
+        #time.sleep(120)
+        r = requests.get(url, headers=self.headers)
+        output = r.json()
+        #validate
+        if output["totalGames"] == 0:
+            raise Exception("No game in match history for last 24h")
+        self.champion_played = output['matches'][0]['champion']
+        output = output['matches'][0]['gameId']
+        return output
+
+    def _get_match_data_by_id(self,match_id):
+        MATCH_URL = f"/lol/match/v4/matches/{match_id}"
+        url = self.API_URL + MATCH_URL
+        r = requests.get(url, headers=self.headers)
+        output = r.json()
+
+        #check for player in game id
+        for player in output['participantIdentities']:
+            if player['player']['accountId'] == self.summoner_id:
+                in_game_id = player['participantId']
+                break
+
+        output = output['participants'][int(in_game_id)-1]
+        return output
+
+#testing class
+obj = ConvertData([0,0])
